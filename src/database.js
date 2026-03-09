739166f23478686561a25c6e0ca3bb75acffb3e1
José David F <90539362+FontalvoJ@users.noreply.github.com>
2026-03-08 22:00:15 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,18 +1,19 @@
+const MONGO_URI =
+  "mongodb+srv://fontalvomejiajosedavid54:dtA9aiNDKmhytlOL@api-node-rentify.omp8p.mongodb.net/dbs_rentify";
+//const LOCAL_URI = "mongodb://localhost:27017/rentify";
 
-//mongoose
-//.connect(
-//"mongodb+srv://fontalvomejiajosedavid54:dtA9aiNDKmhytlOL@api-node-rentify.omp8p.mongodb.net/"
-//)
+export const connectDB = async () => {
+  try {
+    // Conexión a la base de datos
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("🌟 Database is connected");
+  } catch (err) {
+    console.error("❌ Error connecting to the database:", err);
+    process.exit(1);
+  }
+};
 
-
-  import mongoose from "mongoose";
-
-  export const connectDB = async () => {
-    try {
-      await mongoose.connect("mongodb://0.0.0.0:27017/dbs_rentify");
-      console.log("🌟 Database is Connected");
-    } catch (err) {
-      console.error("❌ Error connecting to the database:", err);
-      process.exit(1);
-    }
-  };
+connectDB();
