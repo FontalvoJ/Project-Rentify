4aac7868fbe53f882565308bda6814ceac91d940
José David F <90539362+FontalvoJ@users.noreply.github.com>
2026-04-04 00:48:25 -0500

    Update database

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+/* import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://0.0.0.0:27017/dbs_rentify");
@@ -7,9 +7,8 @@ export const connectDB = async () => {
     console.error("❌ Error connecting to the database:", err);
     process.exit(1);
   }
-};
+};*/
 
-/* 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
@@ -35,4 +34,4 @@ export const connectDB = async () => {
   }
 };
 
-connectDB();*/
+connectDB();
